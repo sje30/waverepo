6dc732782ec6370b9f3891bec130969f7d0ed612
Stephen J. Eglen <sje30@cam.ac.uk>
2014-03-25 20:50:26 +0000

    Update install.package() call for sjemea.
    Macs expect a .tgz (binary package) by default.  So tell it instead to
    install from source.

--- paper/waverepo_installs.R
+++ paper/waverepo_installs.R
@@ -3,14 +3,15 @@
 ## required to run the vignette..
 
 ## 1. Install sjemea
-install.packages(c("./sjemea_0.40.tar.gz"))
+install.packages(pkgs=c("./sjemea_0.40.tar.gz"),
+                 repos = NULL, type="source")
 
 ## It is also available from the web if you wish:
 ##install.packages(c("sjemea"),
 ##                 contriburl="http://damtp.cam.ac.uk/user/eglen/r/")
 
 
-## Install rhdf5 package from Bioconducotr
+## Install rhdf5 package from Bioconductor
 source("http://bioconductor.org/biocLite.R")
 biocLite("rhdf5")
 
